8811799edefe138332cb640758ee19b6540269dc
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-06-22 14:18:39 +0100

    comments on testing

---
+++ note-model.js
@@ -0,0 +1,9 @@
+// what is a module pattern?
+
+// what is the constructer/ prototype pattern needed for this function?
+
+// what the function will do:
+
+//Takes the text of a note upon instantiation e.g. My favourite language is JavaScript.
+//Stores the text in a text property on the note.
+//Has a method that will return the note text.
---
+++ note_test.js
@@ -0,0 +1,32 @@
+// JS it function for tests:
+
+/*
+(function(){
+  'use strict';
+  
+  /**
+   * test function
+   * @param {string} desc
+   * @param {function} fn
+  function it(desc, fn) {
+    try {
+      fn();
+      console.log('\x1b[32m%s\x1b[0m', '\u2714 ' + desc);
+    } catch (error) {
+      console.log('\n');
+      console.log('\x1b[31m%s\x1b[0m', '\u2718 ' + desc);
+      console.error(error);
+    }
+  }
+})();
+*/
+
+// assert function:
+/*
+ function assert(isTrue) {
+    if (!isTrue) {
+      throw new Error();
+    }
+  }
+  */
+
