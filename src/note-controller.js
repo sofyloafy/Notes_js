9104be4f4ea40af3adc2d4c7f538926fc1212bda
Sophie <sophiebrown438@gmail.com>
2020-06-24 13:37:56 +0100

    attempt to update note controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,12 @@
-var element = document.getElementById('app')
-console.log(element.innerHTML)
-element.innerHTML = "Howdy"
+(function(exports) {
+  function NoteController(noteList)
+  this.NoteList.returnList("Favourite pie: pecan")
+console.log(NoteList.returnList())
+
+  NoteController.prototype.NoteToPage = function() {
+    document.getElementById('app').innerHTML = (this.noteListViews.view())
+}
+
+
+exports.NoteController = NoteController
+})(this);
